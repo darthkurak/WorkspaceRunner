0c8984052b99a28a7af148c9751a5adebf87a742
LINGARO\jakub.dropia <jakub.dropia@lingarogroup.com>
2018-12-06 13:21:17 +0100

    Removing start/stop scripts

--- WorkspaceRunner/startScript.ps1
+++
@@ -1,4 +0,0 @@
-Start-Job -ScriptBlock {az vm start -g Lingaro_IoTPlatform_kuba_dev -n jdrotester}
-Start-Job -ScriptBlock {az vm start -g Lingaro_IoTPlatform_kuba_dev -n jdroorleans}
-Start-Job -ScriptBlock {az eventhubs namespace update --name lingarojdro --resource-group Lingaro_IoTPlatform_kuba_dev --capacity 2}
-Get-Job | Wait-Job | Receive-Job
